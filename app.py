6a49173570bc5d414d04f005076383b97ca2fded
Himanshu Mukane <himanshupm18@gmail.com>
2023-11-04 11:54:01 +0530

    changes

--- app.py
+++ app.py
@@ -48,27 +48,6 @@ def predict(image):
 
     return top_5_predictions
 
-class CustomCNN(nn.Module):
-    def __init__(self):
-        super(CustomCNN, self).__init__()
-        self.features = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-        )
-        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
-        self.fc = nn.Linear(128 * 7 * 7, 1000) 
-
-    def forward(self, x):
-        x = self.features(x)
-        x = self.avgpool(x)
-        x = torch.flatten(x, 1)
-        x = self.fc(x)
-        return x
-
 def predict_cnn(image):
     resnet = models.resnet50(pretrained=True)
     transform = transforms.Compose([
